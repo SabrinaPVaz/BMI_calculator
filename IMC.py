f079736a3e8559a3a27383ac53fbe8a21cc7460a
GuilhermeSantos99 <guilhermesanctus6@gmail.com>
2021-08-18 21:00:21 -0300

    Update IMC.py
    Foi adicionado ao código algumas funcionalidade. Agora, é possível verificar em qual faixa do IMC a pessoa se encontra.

--- IMC.py
+++ IMC.py
@@ -1,8 +1,32 @@
 '''Como exercício prático, tente escrever um programa para calcular e informar o IMC (índice de massa corpórea) do usuário, que deverá fornecer seu peso e sua altura. Lembre-se que o IMC é calculado pela fórmula:
  IMC= peso/ altura²'''
 
-peso = eval(input("Digite seu peso"))
-altura = eval(input("Digite sua altura"))
-IMC = peso / altura**2
-print("Seu IMC é:", IMC)
+while True:
+    peso = float(input('Digite o seu peso: '))
+    altura = float(input('Digite a sua altura em metros: '))
+    imc = peso / (altura ** 2)
+    print(f'O seu IMC é: {imc:.1f}.')
+    if 16 > imc < 16.9:
+        print('Resultado: Muito abaixo do peso.')
 
+    elif 17 > imc < 18.4:
+        print('Resultado: Abaixo do peso.')   
+
+    elif 18.5 > imc < 24.9:
+        print('Resultado: Peso normal.')
+
+    elif 25 > imc < 29.9:
+        print('Resultado: Acima do peso.')
+
+    elif 30 > imc < 34.9:
+        print('Resultado: Obesidade Grau I.')
+
+    elif 35 > imc < 40:
+        print('Resultado: Obesidade Grau II.')
+
+    elif imc > 40:
+        print('Resultado: Obesidade Grau III.')
+
+    saída = input('Você deseja calcular novamente? [S/N]: ').strip().upper()[0]
+    if 'N' in saída:
+        break
